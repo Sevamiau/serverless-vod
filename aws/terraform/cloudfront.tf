c106d179f12b6d0b741319f906a95b9daeedfc7d
Sevamiau <sevakunjadas.bms@gmail.com>
2026-09-07 17:32:05 -0300

    feat(terraform): CloudFront distribution over private S3 site bucket

---
+++ aws/terraform/cloudfront.tf
@@ -0,0 +1,50 @@
+resource "aws_cloudfront_distribution" "site_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.site_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.site_oac.id
+    origin_id   = "s3-site-bucket"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CloudFront distribution for site bucket"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-site-bucket"
+  
+  cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6" #instead of forwaded rules
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.site_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "cloudfront.amazonaws.com" }
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.site_bucket.arn}/*"
+      Condition = {
+        StringEquals = { "AWS:SourceArn" = aws_cloudfront_distribution.site_distribution.arn }
+      }
+    }]
+  })
+}
